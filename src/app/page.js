1af0441c7764b3a45a31512515e60842615fd3f9
Masud Parvaz <108426788+masud667@users.noreply.github.com>
2025-09-04 17:10:21 +0600

    create new development brench

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-5xl font-bold">This is My Home Page 🏠</h1>
+      <h1 className="text-5xl font-bold">This is Home Page 🏠</h1>
     </main>
   )
 }
