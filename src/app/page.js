f0371bc53e6e02f259bb69abdb204c6c21b41a67
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-09-05 12:51:04 +0600

    checking

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
       <h1 className="text-5xl font-bold">This is Home Page 🏠</h1>
+      <h1 className="text-5xl font-bold">checking...</h1>
     </main>
   )
 }
