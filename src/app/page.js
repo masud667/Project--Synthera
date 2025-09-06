1fe02f5743f4af005764df84cf20813de6e72d42
Masud Parvaz <108426788+masud667@users.noreply.github.com>
2025-09-06 11:09:14 +0600

    remove ai tryon part

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import TryOn from "./(Components)/Tryon/page";
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-5xl font-bold">This is Home Page 🏠</h1>
+      
       <section>
-        <TryOn></TryOn>
+    hero
       </section>
     </main>
   )
