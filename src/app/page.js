096fbee6089bda609901b32590a604f32fd7f7a5
Masud Parvaz <108426788+masud667@users.noreply.github.com>
2025-09-06 16:46:59 +0600

    added new offer section

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import HeroBanner from "./components/HeroBanner";
 import Categories from "./components/home-compo/Category";
 import TrendingProducts from "./components/home-compo/TrendingProducts";
+import OffersSection from "./components/OffersSection";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <HeroBanner />
       <Categories></Categories>
       <TrendingProducts></TrendingProducts>
+      <OffersSection />
     </>
   )
 }
