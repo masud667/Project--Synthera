a79e0cfce3fc5bc0116f33b8ee93cab3d5756f67
Masud Parvaz <108426788+masud667@users.noreply.github.com>
2025-09-06 15:58:09 +0600

    added new hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Hero from "./components/Hero";
+import HeroBanner from "./components/HeroBanner";
 import Categories from "./components/home-compo/Category";
 import TrendingProducts from "./components/home-compo/TrendingProducts";
 
 export default function Home() {
   return (
     <>
-      <Hero></Hero>
+      <HeroBanner />
       <Categories></Categories>
       <TrendingProducts></TrendingProducts>
     </>
